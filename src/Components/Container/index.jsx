7686dd64e1b8f8e7b444628317d463b66e190532
narradorww <rodrigo.anst@gmail.com>
2021-08-14 20:06:23 -0300

    refatorada toda a estilização,  adicionada a funcionalidade de extrato com expressoes literais

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import Extrato from "../Extrato"
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
-        <Conta />
-      </section>
-    </div>
+      <Conteudo>
+      <Conta />
+      <Extrato/>
+      </Conteudo>
+    </Container>
   );
 };
-
-export default Container;
